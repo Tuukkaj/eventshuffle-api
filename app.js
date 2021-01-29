651f7e84a0e700cf0b7011835a7ff4f4caf7001e
unknown <juusela.tuukka@gmail.com>
2021-01-29 23:28:51 +0200

    Add Express template app and logging middleware

--- app.js
+++ app.js
@@ -1,20 +1,41 @@
 const path = require("path"); 
 require("dotenv").config({path: path.resolve(__dirname, './env/.env')});
 
+const log = new (require("./logger/Logger"))("app"); 
+const loggerMiddleware = require("./logger/loggerMiddleware");
+
+const app = require("express")();
+
+let port = Number(process.env.EXPRESS_PORT); 
+
 const eventsConnection = require("./mongodb/eventsConnection");
 eventsConnection.init();
 
-const log = new (require("./logger/Logger"))("app")
+if(!port) {
+    throw new Error("EXPRESS_PORT in .env is not a number!");
+}
+
 
 function exitHandler() {
     log.log("exitHandler", "Closing connections")
     // Close future connections here
     eventsConnection.closeConnection(); 
+
+    process.exit();
 }
 
 // Exit handlers
 process.on('exit', exitHandler);
 process.on('SIGINT', exitHandler);
-process.on('SIGUSR1', exitHandler);
-process.on('SIGUSR2', exitHandler);
 process.on('uncaughtException', exitHandler);
+
+app.use(loggerMiddleware); 
+
+app.get('/', function(req, res) {
+    res.send('Hello World!')
+
+});
+
+app.listen(port, function() {
+    log.log("app.listen", "Express listening requests at port " + port);
+}); 
