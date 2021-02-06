4dd707ef461694ddf67419c5de16ca39e18b76b4
unknown <juusela.tuukka@gmail.com>
2021-02-06 08:48:53 +0200

    Add comments to event tests

--- test/api/v1/events/events.js
+++ test/api/v1/events/events.js
@@ -19,7 +19,9 @@ const eventsConnection = require("../../../../mongodb/eventsConnection");
 const { assert } = require("chai");
 const { unifyDateString } = require("../../../../routes/api/v1/helpers");
 
+// Before all tests are run 
 before(function(done) {
+    // Run after main application has started
     const onAppLoaded = startedApp => {
         app = startedApp;
         eventRequests = new (require("./EventsRequests"))(app, URL);
@@ -29,6 +31,7 @@ before(function(done) {
     const d = new Date(); 
     const dateString = `${d.getUTCFullYear()}_${d.getUTCMonth()}_${d.getUTCDate()}__${d.getUTCHours()}_${d.getUTCMinutes()}_${d.getUTCSeconds()}_${d.getUTCMilliseconds()}`;
 
+    // Start application and give test database name
     require("../../../../app")(process.env.MONGO_DB_TEST_EVENTS_DATABASE + dateString, onAppLoaded); 
 });
 
@@ -41,7 +44,7 @@ describe("Events API - successful use case", function() {
         body.events.length.should.eql(0, "List's length should be 0 as database is new");
     }); 
 
-    // Test that create works and correct id is returned after
+    // Tests whole case. Create, find, vote and show.
     it("Create, find, vote and show results of event", async () => {
         const redWeddingEvent = testData.successfulUseCase;  
     
@@ -86,6 +89,7 @@ describe("Events API - successful use case", function() {
     }); 
 }); 
 
+// Tests creation of event 
 describe("Events API - create", function() {
     it("Success", async () => {
         const {status, body} = await eventRequests.createEvent(eventTestData.create.success); 
@@ -96,49 +100,51 @@ describe("Events API - create", function() {
 
     it("No name variable", async () => {
         const {status, body} = await eventRequests.createEvent(eventTestData.create.noName); 
-        status.should.eql(400); 
+        status.should.eql(400, "No name should result in error"); 
     }); 
 
     it("Empty name string", async () => {
         const {status, body} = await eventRequests.createEvent(eventTestData.create.emptyName); 
-        status.should.eql(400);
+        status.should.eql(400, "Empty name should result in error");
     }); 
 
     it("No dates", async () => {
         const {status, body} = await eventRequests.createEvent(eventTestData.create.noDates); 
-        status.should.eql(400);
+        status.should.eql(400, "No dates in request should result in error");
     }); 
 
     it("Empty dates array", async () => {
         const {status, body} = await eventRequests.createEvent(eventTestData.create.emptyDates); 
-        status.should.eql(400);
+        status.should.eql(400, "Empty dates array should result in error");
     }); 
 
     it("Wrong type in dates", async () => {
         const {status, body} = await eventRequests.createEvent(eventTestData.create.wrongDateType); 
-        status.should.eql(400);
+        status.should.eql(400, "Wrong type in dates should result in error");
     }); 
 
     it("Two same dates in dates array", async () => {
         const {status, body} = await eventRequests.createEvent(eventTestData.create.twoSameDates); 
-        status.should.eql(400);
+        status.should.eql(400, "Two same dates should result in error");
     }); 
 
     it("Bad date format", async () => {
         const {status, body} = await eventRequests.createEvent(eventTestData.create.badDate); 
-        status.should.eql(400);
+        status.should.eql(400, "Bad date format should result in error");
     });
 
     it("Different date format", async () => {
         const {status, body} = await eventRequests.createEvent(eventTestData.create.differentDateFormat); 
-        status.should.eql(201);
+        status.should.eql(201, "Different date formats should be accepted");
     }); 
 }); 
 
+// Tests showing of event
 describe("Events API - show", function() {
     let eventId; 
     const testItem = eventTestData.show.initialItem; 
 
+    // Creates test item to database
     before(async function() {
         const connection = getConnection().collection(process.env.MONGO_DB_EVENTS_COLLECTION); 
         const created = await connection.insertOne(testItem);
@@ -146,16 +152,17 @@ describe("Events API - show", function() {
         eventId =  created.ops[0]._id;
     });
 
+    // Test that successful use case scenario works
     it("success", async () => { 
         const {status, body} = await eventRequests.showEvent(eventId); 
 
-        status.should.eql(200);
-        body.name.should.eql(testItem.name); 
-        body.id.should.eql(eventId.toString()); 
-        body.dates.length.should.eql(testItem.dates.length); 
+        status.should.eql(200, "Show result be success");
+        body.name.should.eql(testItem.name, "Fetched event's name should be same as given"); 
+        body.id.should.eql(eventId.toString(), "Event IDs should match"); 
+        body.dates.length.should.eql(testItem.dates.length, "Dates array should have same length"); 
 
         const areDatesSame = testItem.dates.every(d => body.dates.includes(d)); 
-        assert(areDatesSame, true); 
+        assert(areDatesSame, true, "Dates should be same"); 
         
         const areVotesSame = testItem.votes.every(vote => {
             return testItem.votes.some(testVote => {
@@ -165,21 +172,22 @@ describe("Events API - show", function() {
             });
         });
 
-        assert(areVotesSame, true);
+        assert(areVotesSame, true, "Votes should be same");
     }); 
 
     it("Bad event ID", async () => {
         const {status, body} = await eventRequests.showEvent("THIS IS NOT AN EVENT ID"); 
 
-        status.should.eql(400); 
+        status.should.eql(400, "Bad ID should result in error"); 
     }); 
 }); 
 
+// Tests voting of event
 describe("Events API - vote", function() {
     let eventId; 
     const { initialItem } = eventTestData.vote;
 
-
+    // Create test item to database
     before(async function() {
         const connection = getConnection().collection(process.env.MONGO_DB_EVENTS_COLLECTION); 
         const created = await connection.insertOne(initialItem);
@@ -187,61 +195,62 @@ describe("Events API - vote", function() {
         eventId = created.ops[0]._id;
     });
 
+    // Tests successful voting scenario
     it("Success", async () => {
         const { successVote } = eventTestData.vote;
         const {status, body} = await eventRequests.voteEvent(eventId, successVote);
-        status.should.eql(200);
+        status.should.eql(200, "Voting should be success");
 
-        body.name.should.eql(initialItem.name);
-        body.dates.length.should.eql(initialItem.dates.length);
-        body.votes.should.a("array");
-        body.votes.length.should.eql(successVote.dates.length);
+        body.name.should.eql(initialItem.name, "Returned name should be same");
+        body.dates.length.should.eql(initialItem.dates.length, "Returned dates length should be same");
+        body.votes.should.a("array", "Votes should be array");
+        body.votes.length.should.eql(successVote.dates.length, "Votes length should be same");
 
         for(const vote of body.votes) {
-            const voteDateIsIncluded = successVote.dates.includes(vote.date);
-            const voterIsIncluded = vote.people.includes(successVote.name);
-            voteDateIsIncluded.should.true;
-            voterIsIncluded.should.true;
+            successVote.dates.includes(vote.date).should.eql(true, "Voted date should be included in votes dates");
+            vote.people.includes(successVote.name).should.eql(true, "Voter's name should be in response");
         }
     });
 
     it("Try to vote with same name two times", async () => {
         const { voteTwiceName } = eventTestData.vote;
         const { status, body } = await eventRequests.voteEvent(eventId, voteTwiceName);
-        status.should.eql(200);
+        status.should.eql(200, "First vote should result in OK");
         const { status: secondStatus, body: secondBody } = await eventRequests.voteEvent(eventId, voteTwiceName);
-        secondStatus.should.eql(404);
+        secondStatus.should.eql(404, "Second vote should result in failure");
     });
 
     it("No name", async () => {
         const { noName } = eventTestData.vote;
         const { status, body } = await eventRequests.voteEvent(eventId, noName);
-        status.should.eql(404);
+        status.should.eql(404, "Vote without name should result in failure");
     }); 
 
     it("No dates", async () => {
         const { noDate } = eventTestData.vote;
         const { status, body } = await eventRequests.voteEvent(eventId, noDate);
-        status.should.eql(404);
+        status.should.eql(404, "Vote without dates should result in failure");
     }); 
 
     it("Date that isn't in event", async () => {
         const { voteDateNotInEvent } = eventTestData.vote;
         const { status, body } = await eventRequests.voteEvent(eventId, voteDateNotInEvent);
-        status.should.eql(404);
+        status.should.eql(404, "Vote with wrong dates should result in failure");
     });
 
     it("Two same dates in vote", async () => {
         const { twoSameDates } = eventTestData.vote;
         const { status, body } = await eventRequests.voteEvent(eventId, twoSameDates);
-        status.should.eql(404);
+        status.should.eql(404, "Two same dates should result in failure");
     });
 }); 
 
+// Tests showing of results in event
 describe("Events API - result", function() {
     let successEventId, noVotesId; 
     const {success, successSuitableDates, noVotes} = eventTestData.result; 
 
+    // Init test items to database
     before(async function() {
         const connection = getConnection().collection(process.env.MONGO_DB_EVENTS_COLLECTION); 
 
@@ -252,32 +261,32 @@ describe("Events API - result", function() {
         noVotesId = noVotesCreated.ops[0]._id;
     }); 
 
+    // Successful use case scenario
     it("Success", async () => {
         const {status, body} = await eventRequests.resultEvent(successEventId);
-        status.should.eql(200);
-        body.name.should.eql(success.name);
-        body.suitableDates.length.should.eql(successSuitableDates.length);
+        status.should.eql(200, "Showing results should result in OK");
+        body.name.should.eql(success.name, "Returned name should equal created item's");
+        body.suitableDates.length.should.eql(successSuitableDates.length, "Suitable dates lengths should be same");
         
-        const suitableDatesAreCorrect = body.suitableDates.every(d => successSuitableDates.includes(d.date));
-        suitableDatesAreCorrect.should.true; 
+        body.suitableDates.every(d => successSuitableDates.includes(d.date)).should.eql(true, "All suitable dates should be included in response");
     });
 
     it("No votes", async () => {
         const {status, body} = await eventRequests.resultEvent(noVotesId);
-        status.should.eql(204);
+        status.should.eql(204, "Showing results in event that has no votes should result in 204");
     });
 
     it("Bad event ID", async () => {
         const {status, body} = await eventRequests.showEvent("THIS IS NOT AN EVENT ID"); 
 
-        status.should.eql(400); 
+        status.should.eql(400, "Giving bad ID should result in failure"); 
     }); 
 });
 
 describe("Events API - list", function() {
-    let eventId; 
     let events; 
 
+    // Fetches all of the events from database
     before(async function () {
         const connection = getConnection().collection(process.env.MONGO_DB_EVENTS_COLLECTION); 
         events = await connection.find({}).toArray(); 
@@ -285,18 +294,17 @@ describe("Events API - list", function() {
     
     it("All events are listed", async () => {
         const {status, body} = await eventRequests.listEvents();
-        body.events.length.should.eql(events.length);
+        body.events.length.should.eql(events.length, "Returned event list should have same length");
         
         for(const e of events) {
-            const isEventInList = body.events.some(bodyEvent => {
+            body.events.some(bodyEvent => {
                 return bodyEvent.name === e.name && bodyEvent.id === e._id.toString();
-            });
-            
-            isEventInList.should.true;
+            }).should.eql(true, "All events should be listed");
         }
     });
 }); 
 
+// Closes connection to test database. Also drobs test database if configured in .env
 after(async function() {
     let connection = getConnection(); 
     let client = getClient(); 
